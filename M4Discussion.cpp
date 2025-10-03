3b062f54bd9543d9b1a683c181c5eb3f0ed3585d
pMartinez <perry.martinez.cy@gmail.com>
2025-10-02 20:55:51 -0400

    Add files via upload

---
+++ M4Discussion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int min(i){
+    int num = 0;
+    for(x = 0;x<3;x++){
+        while(num = 1){
+            cout<<"hello world\n"<<endl;
+            --x;
+        }
+    }
+    return 0
+}
